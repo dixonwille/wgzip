802947693b070cde35bcbb153409cc37642480fb
Will Dixon <will.dixon@acstechnologies.com>
2016-04-19 22:23:37 -0400

    Returned unziped path

--- wgzip.go
+++ wgzip.go
@@ -35,26 +35,26 @@ func Gzip(source, target string) error {
 
 //UnGzip unzips the source to target.
 //Uses archive name for file.
-func UnGzip(source, target string) error {
+func UnGzip(source, target string) (string, error) {
 	reader, err := os.Open(source)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer reader.Close()
 
 	archive, err := gzip.NewReader(reader)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer archive.Close()
 
 	target = filepath.Join(target, archive.Name)
 	writer, err := os.Create(target)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer writer.Close()
 
 	_, err = io.Copy(writer, archive)
-	return err
+	return target, err
 }
